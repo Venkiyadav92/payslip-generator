121341d7755ddeb24917fd79b1be5ef22478769a
Venkiyadav92 <vyvenkateshyadav@gmail.com>
2025-05-08 22:24:02 +0530

    changes

--- backend/utils/calculator.js
+++ backend/utils/calculator.js
@@ -1,11 +1,10 @@
 module.exports = function calculate(data) {
-    const workingDays = 30;
-    const perDay = data.basicSalary / workingDays;
-    const basicPay = perDay * data.daysPresent;
-    const allowances = 1000; // default allowance
-    const deductions = (workingDays - data.daysPresent) * perDay;
-    const netPay = basicPay + allowances - deductions;
-  
-    return { basicPay, allowances, deductions, netPay };
-  }
-  
+  const workingDays = 30;
+  const perDay = data.basicSalary / workingDays;
+  const basicPay = perDay * data.daysPresent;
+  const allowances = 1000; // default allowance
+  const deductions = (workingDays - data.daysPresent) * perDay;
+  const netPay = basicPay + allowances - deductions;
+
+  return { basicPay, allowances, deductions, netPay };
+}
